6c8ca682b63031a1d403fbedf919461596c0401d
TrianePeart <TrianePeart@gmail.com>
2022-09-19 15:40:41 -0400

    Save Point - 17 Passing

--- main.js
+++ main.js
@@ -1,14 +1,70 @@
+const url = "https://ghibliapi.herokuapp.com/films"
+const select = document.querySelector('select')
+//this might be dropdown if it doesn't work
+const ul = document.querySelector('ul')
+const display = document.querySelector('#display-info')
+const review = document.querySelector('#review')
+const button = document.querySelector('#reset-reviews')
+const form = document.querySelector('form')
 
-
-
-
-
-
-// To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
+let allow
+let reviews = review.value
+let movies = select.value
 
 function run() {
- // Add code you want to run on page load here
+ fetch(url)
+ .then((res) => res.json())
+ .then((data) => {
+    allow = data
+    //erase if anything 
+
+    data.forEach((movie) =>{
+        let option = document.createElement('option')
+        option.value = movie.title
+        option.innerHTML = movie.title
+        select.append(option)
+    })
+ })
+ .catch(error =>{
+    console.log(error)
+ });
+ 
+ //EVENT LISTENERS MIGHT HAVE TO BE OUTSIDE CODE
+    
+    form.addEventListener('submit', (event) => {
+        event.preventDefault()
+        const li = document.createElement('li')
+        li.innerHTML = `<strong><b>${select.value}</b></strong>: ${review.value}`
+        ul.append(li)
+        if(!movies){
+            alert('Please select a movie first')
+        }else{
+            li.innerHTML =`<b>${movieName.innerText}.</b>: ${review.value}`
+        }
+        if(!reviews){
+            alert('Please input a review')
+        }
+        //NOT WORKING Figure out what to put here later 
+        form.reset();
+    });
+
+    // select.addEventListener('change', (event) =>{
+    //     event(preventDefault)()
+       
+    // })
+    
+    button.addEventListener('click', () => {
+         ul.innerHTML = '';
+      });
 }
+
+ 
+
+
+
+
+// CODE NOT WORKING. WHEN SOMETHING IS POPULATED TRY TO REVAMP. IF Not Working Keep to ask why. 
+
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
 // So that testing can work as expected for now
