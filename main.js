b1a04d6ed75339072248305207b7a481b751fe8e
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-09-19 11:40:32 -0400

    initial html and css

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
-
-
-
-
-
-
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
 function run() {
- // Add code you want to run on page load here
+  // Add code you want to run on page load here
 }
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
