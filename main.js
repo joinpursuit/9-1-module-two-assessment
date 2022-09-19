b9ba9cad4a567bee7678b9c6ca80854a3e50476f
Jonathan-Gaal <jonathangaal@pursuit.org>
2022-09-19 13:32:21 -0400

    got film details outputting

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const selectMovie = document.querySelector('#titles');
+let displayInfo = document.querySelector("#display-info");
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 function fetchData(URL) {
     
@@ -14,10 +15,36 @@ function fetchData(URL) {
           option.value = `${film.id}`;
             option.innerText = `${film.title}`;
             console.log(option.value)
-            selectMovie.append(option);
+          selectMovie.append(option);
+          
+          // create an h3 with movie title, a p tag with movie release year, and a p tag with description
+
+           selectMovie.addEventListener("change", (e) => {
+            //  e.preventDefault();
+             console.log('hey')
+             
+             let movieDetailsH3 = document.createElement("h3");
+             let movieDetailsReleaseYear = document.createElement("p");
+             let movieDetailsDescription = document.createElement("p");
+             if (selectMovie.value === film.id) {
+               movieDetailsH3.innerText = `${film.title}`;
+               movieDetailsReleaseYear.innerText = `${film.release_date}`;
+               movieDetailsDescription.innerText = `${film.description}`;
+               
+               displayInfo.append(
+                 movieDetailsH3,
+                 movieDetailsReleaseYear,
+                 movieDetailsDescription
+               );
+             }
+           });
+
+
+
 
             
         });
+       
       })
       .catch((err) => {
         console.error(err);
