327c6f602be3bd3b24fd5ac3c3542004ad3b7c01
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-09-19 14:21:28 -0400

    cypress not passing

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const baseURL = `https://ghibliapi.herokuapp.com`;
 
 let filmData;
 let film;
+let peopleData;
 
 const selectTitle = document.querySelector(`select`);
 const movieDetails = document.querySelector(`#display-info`);
@@ -9,6 +10,8 @@ const inputReview = document.querySelector(`#review`);
 const form = document.querySelector(`form`);
 const reviewList = document.querySelector(`ul`);
 const reset = document.querySelector(`#reset-reviews`);
+const showPeople = document.querySelector(`#show-people`);
+const peopleList = document.querySelector(`ol`);
 
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
@@ -40,37 +43,42 @@ const findFilm = () => {
 
 selectTitle.addEventListener(`change`, () => {
   movieDetails.innerHTML = ``;
-  const title = document.createElement(`h3`);
+  const titleFilm = document.createElement(`h3`);
   const year = document.createElement(`p`);
   const description = document.createElement(`p`);
 
   findFilm();
-  title.innerText = film.title;
+  titleFilm.innerText = film[`title`];
   year.innerText = film[`release_date`];
   description.innerHTML = film.description;
 
-  movieDetails.append(title, year, description);
+  movieDetails.append(titleFilm, year, description);
+  peopleList.innerHTML = ``;
 });
 
 form.addEventListener(`submit`, (event) => {
   event.preventDefault();
   if (!selectTitle.value) {
-    const noFilm = document.createElement(`p`);
-    noFilm.innerHTML = `<strong>Please select a movie fist</strong>`;
-    form.after(noFilm);
-    const removeErrMess = () => noFilm.remove();
-    setTimeout(removeErrMess, 4000);
-  } else if (!inputReview.value) {
-    const noReview = document.createElement(`p`);
-    noReview.innerHTML = `<strong>Please add a review</strong>`;
-    form.after(noReview);
-    const removeErrMess = () => noReview.remove();
-    setTimeout(removeErrMess, 4000);
+    alert(`Please select a movie first`);
+    // other way to display error message
+    // const noFilm = document.createElement(`p`);
+    // noFilm.innerHTML = `<strong>Please select a movie fist</strong>`;
+    // form.after(noFilm);
+    // const removeErrMess = () => noFilm.remove();
+    // setTimeout(removeErrMess, 4000);
+
+    // could add another error message
+    //   } else if (!inputReview.value) {
+    // const noReview = document.createElement(`p`);
+    // noReview.innerHTML = `<strong>Please add a review</strong>`;
+    // form.after(noReview);
+    // const removeErrMess = () => noReview.remove();
+    // setTimeout(removeErrMess, 4000);
   } else {
     const reviewItem = document.createElement(`li`);
-    console.log(selectTitle.target);
-    reviewItem.innerHTML =
-      `<strong>${film.title}: </strong>` + inputReview.value;
+    reviewItem.innerHTML = `<strong>${film[`title`]}: </strong>${
+      inputReview.value
+    }`;
     reviewList.append(reviewItem);
     form.reset();
   }
@@ -78,4 +86,32 @@ form.addEventListener(`submit`, (event) => {
 
 reset.addEventListener(`click`, () => {
   reviewList.innerHTML = ``;
+});
+
+fetch(baseURL + `/people`)
+  .then((res) => res.json())
+  .then((resJson) => {
+    peopleData = resJson;
+  })
+  .catch((err) => console.log(err));
+
+showPeople.addEventListener(`click`, () => {
+  peopleList.innerHTML = ``;
+  if (selectTitle.value) {
+    // console.log(selectTitle.value);
+    peopleData.forEach((person) => {
+      person.films.forEach((film) => {
+        if (film.includes(selectTitle.value)) {
+          const character = document.createElement(`li`);
+          character.innerText = person.name;
+          peopleList.append(character);
+        }
+      });
+    });
+
+    // fetch(baseURL + `/items` + `?films=${baseURL}/films/${selectTitle.value}`)
+    //   .then((res) => res.json())
+    //   .then((resJson) => console.log(resJson))
+    //   .catch((err) => console.log(err));
+  }
 });
