97a7fb59b9aae3bbe9efe3a35c5106234c148e5f
melanieinpursuit <melaniegonzalez@pursuit.org>
2022-09-19 18:00:57 -0400

    finished options and details

--- main.js
+++ main.js
@@ -8,6 +8,39 @@
 
 function run() {
  // Add code you want to run on page load here
+
+ const baseAPI = 'https://ghibliapi.herokuapp.com/Films'
+ const dropDownMenu = document.querySelector('select')
+
+ fetch(baseAPI)
+ .then(response => response.json())
+ .then(data => {
+    console.log(data)
+
+    for (let i = 0; i < data.length; i++) {
+        let newOption = document.createElement('option')
+        newOption.innerHTML = data[i].title
+        dropDownMenu.append(newOption)
+    }
+
+    const filmYear = document.querySelector(".filmYear")
+    const filmDescription = document.querySelector(".filmDescription")
+    const movieDetails = document.querySelector(".details")
+
+    dropDownMenu.addEventListener("change", (event) => {
+        console.log("change") // Test to check change event listener.
+
+        event.preventDefault()
+
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].title === event.target.value) {
+                filmYear.innerHTML = `${data[i].release_date}`
+                filmDescription.innerHTML = `${data[i].description}`
+            }
+        }
+        movieDetails.append(filmYear, filmDescription)
+    })
+ })
 }
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
