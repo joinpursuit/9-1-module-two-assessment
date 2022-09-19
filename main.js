71f7738cb8d9cc86a2d01d01ca2d4474a453995c
sreeves07 <shamarreeves@pursuit.org>
2022-09-19 13:11:10 -0400

    drop down menu & description

--- main.js
+++ main.js
@@ -1,14 +1,44 @@
-
-
-
-
-
+const BASE_URL = 'https://ghibliapi.herokuapp.com/films'
+const selectMenu = document.querySelector('select')
+const movieOption = document.querySelector('option')
+const displayInfo = document.querySelector('#display-info')
 
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
 function run() {
- // Add code you want to run on page load here
+    fetch(BASE_URL)
+        .then((res) => res.json())
+        .then((data) => {
+            // console.log(data)
+            data.forEach((element) => {
+                const option = document.createElement('option')
+                option.value = element.id
+                option.textContent = element.title
+                selectMenu.append(option)
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
 }
+
+document.getElementById('titles').addEventListener('change', function() {
+    fetch(`${BASE_URL}/${this.value}`)
+        .then((res) => res.json())
+        .then((data) => {
+            // console.log(data)
+
+            displayInfo.innerHTML = `
+            <h3>${data.title}</h3>\n
+            <p>${data.release_date}</p>\n
+            <p>${data.description}</p>`
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+  });
+
+
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
 // So that testing can work as expected for now
