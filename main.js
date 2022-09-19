b38fe47bbbd693b204a6ed3c7d759e4559e010ee
Gillesedgy <edgygilles@pursuit.org>
2022-09-19 15:10:06 -0400

    Reset button created

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const header = document.querySelector('header')
+const header = document.querySelector("header");
 const form = document.querySelector("form");
-const selectTitles = document.querySelector("#titles");
+const selectTitles = document.querySelector("#title");
 const display = document.querySelector("#display-info");
 const reviewInput = document.querySelector("#reviews");
 const details = document.querySelector("#movie-details");
@@ -8,8 +8,8 @@ const ul = document.querySelector("ul");
 const input = document.querySelector("#review");
 console.log(input);
 const revDisplay = document.querySelector("#reviews ul");
-selectInfo = document.querySelector("#select-info")
-// const form = document.querySelector("form")
+selectInfo = document.querySelector("#select-info");
+const reset = document.querySelector("#reset-reviews")
 // const form = document.querySelector("form")
 // const form = document.querySelector("form")
 
@@ -23,6 +23,7 @@ function run() {
     .then((resJson) => {
       apiResult = resJson;
       console.log(apiResult);
+
       apiResult.forEach((film) => {
         let selectOpt = document.createElement("option");
         selectOpt.innerHTML = film.title;
@@ -34,6 +35,7 @@ function run() {
     })
     .catch((error) => console.log(error));
   //* Movie Description
+
   selectTitles.addEventListener("change", () => {
     //! remember to clear secstion after seleting another movie
     display.innerHTML = "";
@@ -41,8 +43,9 @@ function run() {
       .then((res) => res.json())
       .then((resJson) => {
         // console.log(resJson)
+        // const desc = apiResult.find((info) => info.title === selectTitles.value);
         const titleH3 = document.createElement("h3");
-        titleH3.innerHTML = resJson.title;
+        titleH3.innerHTML = `${resJson.title}`;
 
         const year = document.createElement("p");
         year.innerHTML = resJson.release_date;
@@ -55,27 +58,31 @@ function run() {
       })
       .catch((err) => console.log(err));
   });
+
   //* Add Reviews
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     form.reset();
-    if(!selectTitles.value){
-        // const errP = Document.createElement("p")
-       alert("Please select a movie first")
+    if (!selectTitles.value) {
+      // const errP = Document.createElement("p")
+      alert("Please select a movie first");
 
-        selectTitles.addEventListener("change", ()=>{
-            // errP.innerHTML=``
-            header.append(errP)
-        })
+      selectTitles.addEventListener("change", () => {
+        // errP.innerHTML=``
+        header.append(errP);
+      });
+    } else {
+      const reviews = document.createElement("li");
+      ul.append(reviews);
+      reviews.innerHTML = `<strong>${selectTitles.value}:</strong> ${input}`;
+      ul.append(revDisplay);
     }
-    else {
-    const reviews = document.createElement("li");
-    ul.append(reviews);
-    reviews.innerHTML = `<strong>${selectTitles.value}:</strong> ${input}`;
-    ul.append(revDisplay)
-    }
-
   });
+
+  //* Reset button
+reset.addEventListener('click', ()=>{
+revDisplay.innerHTML=''
+})
 }
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
