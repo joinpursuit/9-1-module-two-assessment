d57a006d40ce1a6131fde4af423b4924b3407e5c
Gillesedgy <edgygilles@pursuit.org>
2022-09-19 17:03:57 -0400

    passing 6.5

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const header = document.querySelector("header");
 const form = document.querySelector("form");
 const selectTitles = document.querySelector("#title");
 const display = document.querySelector("#display-info");
+console.log(display)
 const reviewInput = document.querySelector("#reviews");
 const details = document.querySelector("#movie-details");
 const ul = document.querySelector("ul");
@@ -25,6 +26,7 @@ function run() {
       console.log(apiResult);
 
       apiResult.forEach((film) => {
+
         let selectOpt = document.createElement("option");
         selectOpt.innerHTML = film.title;
         //! add Id value in the dom
@@ -35,15 +37,17 @@ function run() {
     })
     .catch((error) => console.log(error));
   //* Movie Description
-
+let title;
   selectTitles.addEventListener("change", () => {
     //! remember to clear secstion after seleting another movie
-    display.innerHTML = "";
+    display.innerHTML=''
     fetch(`https://ghibliapi.herokuapp.com/films/${selectTitles.value}`)
       .then((res) => res.json())
       .then((resJson) => {
-        // console.log(resJson)
-        // const desc = apiResult.find((info) => info.title === selectTitles.value);
+       
+        title=resJson.title
+        //! fix, find method not working
+        // const desc = apiResult.find((info) => info.title === selectTitles.value); 
         const titleH3 = document.createElement("h3");
         titleH3.innerHTML = `${resJson.title}`;
 
@@ -54,7 +58,7 @@ function run() {
         description.innerHTML = resJson.description;
         console.log(description);
 
-        details.append(titleH3, year, description);
+       display.append(titleH3, year, description);
       })
       .catch((err) => console.log(err));
   });
@@ -62,23 +66,18 @@ function run() {
   //* Add Reviews
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    form.reset();
+   
     if (!selectTitles.value) {
       // const errP = Document.createElement("p")
       alert("Please select a movie first");
-
-      selectTitles.addEventListener("change", () => {
-        // errP.innerHTML=``
-        header.append(errP);
-      });   
-         ul.append(revDisplay);
+        //  ul.append(revDisplay);
     }
      else {
       const reviews = document.createElement("li");
       ul.append(reviews);
-      reviews.innerHTML = `<strong>${selectTitles.value}:</strong> ${input}`;
-
+      reviews.innerHTML = `<strong>${title}:</strong> ${input.value}`;
     }
+    form.reset();
   });
 
   //* Reset button
