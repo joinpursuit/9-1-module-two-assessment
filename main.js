3e45a60ae246fcd7908f7bbf6a3bfc4b2fd875d4
chrisking718 <chrisking@pursuit.org>
2022-09-19 11:55:31 -0400

    fetch works, loads up list with movies

--- main.js
+++ main.js
@@ -7,6 +7,30 @@
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
 function run() {
+    fetch("https://ghibliapi.herokuapp.com/films")
+    .then((res)=>res.json())
+    .then((res)=>{
+       console.log(res)
+       
+       
+       let i  = 0
+       res.forEach(element => {
+           
+           const opt = document.createElement("option")
+           opt.setAttribute("value",`${element.title}`)
+           opt.innerText=`${element.title}`
+           
+   
+           movies.append(opt)
+   
+          
+           i++ 
+       });
+   
+       window.localStorage.setItem('person',JSON.stringify(res))
+       
+   })
+    .catch((error) => console.log(error))
  // Add code you want to run on page load here
 }
 
