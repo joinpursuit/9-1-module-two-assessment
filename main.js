2a5f8703dbe553c69fb3b9fbb6b848a3a7b0816c
Gillesedgy <edgygilles@pursuit.org>
2022-09-19 13:58:52 -0400

    working on more Details

--- main.js
+++ main.js
@@ -1,57 +1,81 @@
-const form = document.querySelector("form")
-const selectTitles = document.querySelector("#titles")
-const reviewInput = document.querySelector("#reviews")
-const display= document.querySelector("#display-info")
+const header = document.querySelector('header')
+const form = document.querySelector("form");
+const selectTitles = document.querySelector("#titles");
+const display = document.querySelector("#display-info");
+const reviewInput = document.querySelector("#reviews");
+const details = document.querySelector("#movie-details");
+const ul = document.querySelector("ul");
+const input = document.querySelector("#review");
+console.log(input);
+const revDisplay = document.querySelector("#reviews ul");
+selectInfo = document.querySelector("#select-info")
 // const form = document.querySelector("form")
 // const form = document.querySelector("form")
 // const form = document.querySelector("form")
-// const form = document.querySelector("form")
-// const form = document.querySelector("form")
-
-
-
-
 
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 let apiResult;
-const URL = 'https://ghibliapi.herokuapp.com/films'
+const URL = "https://ghibliapi.herokuapp.com/films";
 function run() {
-    fetch(URL)
-    .then((result)=> result.json())
-    .then((resJson)=>{
-      apiResult = resJson
-      console.log(apiResult)
-apiResult.forEach((film) => {
- let selectOpt = document.createElement("option") 
- selectOpt.innerHTML= film.title
- selectOpt.value= film.id
- selectTitles.append(selectOpt)
-})
+  //*Select a Movie Section
+  fetch(URL)
+    .then((result) => result.json())
+    .then((resJson) => {
+      apiResult = resJson;
+      console.log(apiResult);
+      apiResult.forEach((film) => {
+        let selectOpt = document.createElement("option");
+        selectOpt.innerHTML = film.title;
+        //! add Id value in the dom
+        selectOpt.value = film.id;
+        // selectOpt.setAttribute("value", film.id)
+        selectTitles.append(selectOpt);
+      });
     })
-.catch((error)=> console.log(error))
+    .catch((error) => console.log(error));
+  //* Movie Description
+  selectTitles.addEventListener("change", () => {
+    //! remember to clear secstion after seleting another movie
+    display.innerHTML = "";
+    fetch(`https://ghibliapi.herokuapp.com/films/${selectTitles.value}`)
+      .then((res) => res.json())
+      .then((resJson) => {
+        // console.log(resJson)
+        const titleH3 = document.createElement("h3");
+        titleH3.innerHTML = resJson.title;
 
+        const year = document.createElement("p");
+        year.innerHTML = resJson.release_date;
 
-selectTitles.addEventListener("change", ()=>{
-    //! remember to clear secstion after seleting another movie 
-fetch(`https://ghibliapi.herokuapp.com/films/${selectTitles.value}`)
- .then((res)=> res.json()) 
- .then((resJson)=>{
-    // console.log(resJson)
-const titleH3 = document.createElement('h3')
-titleH3.innerHTML= resJson.title
+        const description = document.createElement("p");
+        description.innerHTML = resJson.description;
+        console.log(description);
 
-const year = document.createElement('p')
-year.innerHTML= resJson.release_date
+        details.append(titleH3, year, description);
+      })
+      .catch((err) => console.log(err));
+  });
+  //* Add Reviews
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    form.reset();
+    if(!selectTitles.value){
+        // const errP = Document.createElement("p")
+       alert("Please select a movie first")
 
-const description = document.createElement('p')
-description.innerHTML=resJson.description
-console.log(description)
+        selectTitles.addEventListener("change", ()=>{
+            // errP.innerHTML=``
+            header.append(errP)
+        })
+    }
+    else {
+    const reviews = document.createElement("li");
+    ul.append(reviews);
+    reviews.innerHTML = `<strong>${selectTitles.value}:</strong> ${input}`;
+    ul.append(revDisplay)
+    }
 
-display.append(titleH3, year, description)
- })
-.catch((err)=> console.log(err))
-
-})
+  });
 }
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
