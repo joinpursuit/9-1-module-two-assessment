3f936356a386fc97954d088af3b309038f50735d
Salinamalek <salinamalek@pursuit.org>
2022-09-19 11:18:00 -0400

    select menu done

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+const url = "https://ghibliapi.herokuapp.com/films"
 
 
 
@@ -8,6 +8,20 @@
 
 function run() {
  // Add code you want to run on page load here
+ fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+        for(let i = 0; i < data.length; i++){
+            const title = data[i].title;
+            // console.log(title)
+            const option = document.createElement('option');
+            option.textContent = title;
+            option.value = title;
+            const select = document.querySelector('select');
+            select.append(option);
+        }
+    })
+    .catch((err) => console.log(err))
 }
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
