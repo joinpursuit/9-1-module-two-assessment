78e9c0863e2c5d9fdccb44ed7ae3ca486813a200
Jonathan-Gaal <jonathangaal@pursuit.org>
2022-09-19 11:39:02 -0400

    header complete

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
 
-
-
-
-
-
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
+function fetchData(URL){
+    fetch(URL)
+      .then((res) => res.json())
+      .then((resJson) => {
+        console.log(resJson);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
 
 function run() {
+    const URL = "https://ghibliapi.herokuapp.com/films";
+    
+    fetchData(URL);
+
+
+
+   
+
+   
  // Add code you want to run on page load here
+   
+   
 }
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
