c2c63bb44d428ca7a07bf41acd2faba3a30bb0c2
Richie-Pursuit <richiecarrasco@pursuit.org>
2022-09-20 02:31:52 -0400

    15 passing

--- main.js
+++ main.js
@@ -1,3 +1,54 @@
+const selecting = document.querySelector(`#titles`)
+const form=document.querySelector('form')
+const movieTitle=document.querySelector('.name')
+const info=document.querySelector('.description')
+const date=document.querySelector('.date')
+const URL = `https://ghibliapi.herokuapp.com/films`
+
+
+
+    fetch(`${URL}`)
+    .then((res) => res.json())
+    .then((resJson)=>{
+        
+        resJson.forEach(({id,title})=> {
+            const option=document.createElement('option')
+            x=title
+            option.innerText=`${title}`
+            option.value=`${id}`
+            selecting.append(option)
+            
+        });
+
+    })
+    .catch(error=>console.log(error))
+
+    selecting.addEventListener('change',()=>{
+        fetch(`${URL}/${selecting.value}`)
+        .then((res)=>res.json())
+        .then((resJson)=> {
+            // const movie= resJson.title
+            movieTitle.innerHTML=`${resJson.title}`
+            info.innerHTML=`${resJson.description}`
+            date.innerHTML=`${resJson.release_date}`
+    })
+        .catch(error=>console.log(error))
+    })
+
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault()
+        fetch(`${URL}/${selecting.value}`)
+        .then((res)=>res.json())
+        .then((resJson)=>{
+            const revTitle= resJson.title
+            const rev=e.target.review.value
+            list(revTitle, rev)
+            form.reset()
+        })
+        
+
+    })
+
 
 
 
@@ -6,7 +57,8 @@
 
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
-function run() {
+function run(titles) {
+
  // Add code you want to run on page load here
 }
 
