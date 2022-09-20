d0432569dd150bc2c478be3568bbbaf7dffb77f8
Salinamalek <salinamalek@pursuit.org>
2022-09-20 11:23:09 -0400

    19/22 tests passing - 6/8 features  passing

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
-const url = "https://ghibliapi.herokuapp.com/films"
+const films_url = "https://ghibliapi.herokuapp.com/films"
 const select = document.querySelector('select');
 const form = document.querySelector('form');
-const body = document.querySelector('body')
+const body = document.querySelector('body');
 let recallData;
-
 
 
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
 function run() {
  // Add code you want to run on page load here
- fetch(url)
+
+ // populates dropdown menu with film names
+ fetch(films_url)
     .then((res) => res.json())
     .then((data) => {
         recallData = data
         for(let i = 0; i < data.length; i++){
             const title = data[i].title;
             // console.log(title)
-            const id = data[i].id
             const option = document.createElement('option');
             option.textContent = title;
             option.value = title;
-            // const select = document.querySelector('select');
             select.append(option);
         }
     })
     .catch((err) => console.log(err));
 
+
     select.addEventListener('change', (event) => {
         event.preventDefault();
+        // dataFind is a variable used to store the films data (recallData) to use later
         const dataFind = recallData.find((el) => el.title === `${select.value}`);
         //  console.log(dataFind)
         const info = document.querySelector('#display-info');
@@ -36,6 +37,7 @@ function run() {
         const h3 = document.createElement('h3')
         const p = document.createElement('p')
         const p2 = document.createElement('p')
+        // movie details section gets populated with movie description depending on the film sleected
         h3.textContent = `${dataFind.title}`
         p.textContent = `${dataFind.release_date}`
         p2.innerHTML = `${dataFind.description}`
@@ -44,25 +46,29 @@ function run() {
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
+        // add reviews based on the film selected
         const name = `${select.value}`;
         const review = document.querySelector('#review')
         const reviewVal = review.value
-
         const list = document.createElement('li')
         list.innerHTML = `<strong>${name}: </strong>${reviewVal}`;
         const ul = document.querySelector('ul') 
         ul.append(list);
-
-        
+        // clears input after submission of review
         form.reset();
-
+        
+        // resets the reviews (empties the ul content)
         const button = document.querySelector('#reset-reviews')
         button.addEventListener('click', () => {
-        ul.innerHTML = ''
-        });
-    })
-}
+            ul.innerHTML = ''
+        }); 
 
+        // error (alert) if review was submitted WITHOUT selecting a film
+        if(select.value === ''){
+            window.alert(`Please select a movie first`)
+        }; 
+    })
+};
 
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
