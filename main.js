a083f10f238da5b2ec9259055ffcaf7a4afefea1
Jonathan-Gaal <jonathangaal@pursuit.org>
2022-09-20 11:25:43 -0400

    submission ready

--- main.js
+++ main.js
@@ -1,9 +1,72 @@
 const selectMovie = document.querySelector('#titles');
 let displayInfo = document.querySelector('#display-info');
 let reviewsUl = document.querySelector('#reviews-ul');
-addReviewForm = document.querySelector('#add-review-form');
-reviewTextBox = document.querySelector('#review');
+const addReviewForm = document.querySelector('#add-review-form');
+let reviewTextBox = document.querySelector('#review');
 const resetReviewsBtn = document.querySelector("#reset-reviews");
+const showPeopleBtn = document.querySelector("#show-people");
+const peopleOl = document.querySelector('#people-ol')
+
+
+//lines 12 - 66 please ignore, dont want to comment them out because I want to work on this on my own, and if i comment it out i may not remember what to comment back in... Thanks
+function people2() {
+  const URL = "https://ghibliapi.herokuapp.com/people";
+}
+
+
+
+
+
+
+
+
+function people(URL) {
+  
+  
+  fetch(URL)
+    .then((res) => res.json())
+    .then((resJson) => {
+      console.log(resJson);
+
+      resJson.forEach((personLayer1, i) => {
+        console.log(i, "personLayer1 -- entire array", personLayer1.people);
+
+        personLayer1.people.forEach((personLayer2, i) => {
+        console.log(i, 'singular personLayer2', personLayer2)
+
+        // personLayer2.forEach((personLayer3, i) => {
+        //   console.log(i, 'person layer3', personLayer3 )
+        // })
+
+        let URLPerson = personLayer2;
+        // console.log('url person', URLPerson)
+        fetch(URLPerson)
+          .then((res) => res.json())
+          .then((resJson) => {
+            // console.log("person resJson", resJson.name); //--> a persons name
+              // console.log("person resJson", resJson.films);
+
+
+            
+              let peopleLi = document.createElement("li");
+              peopleLi.innerText = resJson.name;
+              peopleOl.append(peopleLi);
+            
+
+          }); // closes inner fetch
+      }); //closes personLayer2 forEach
+
+      });//closes personLayer1 forEach
+    }) //closes  people Fetch
+
+    .catch((err) => {
+      console.error(err);
+    }); //closes people catch
+} // closes people function
+
+
+
+
 
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 function fetchData(URL) {
@@ -12,7 +75,7 @@ function fetchData(URL) {
     fetch(URL)
       .then((res) => res.json())
       .then((resJson) => {
-        console.log(resJson);
+      // console.log(resJson)
 
         //Create an option in dropdown menu for each element -- value = film id, text = film title
         resJson.forEach(film => {
@@ -20,7 +83,7 @@ function fetchData(URL) {
           option.value = `${film.id}`;
           option.innerText = `${film.title}`;
           selectMovie.append(option);
-          
+
 
           // Adds change event listener to dropdown, and creates an h3 with movie title, p tag with movie release year, and p tag with film description.
 
@@ -41,7 +104,7 @@ function fetchData(URL) {
                 movieDetailsDescription
               );
             } // closes movie details IF
-          }); // closes selectMovie change event listener
+          }); // closes selectMovie change-event 
 
           
         }); // closes forEach
@@ -66,30 +129,24 @@ function fetchData(URL) {
           }); // closes forEach to find title by id (from dropdown.value)
           // reviewsUl.remove(reviewListItem);
           
-        }); //closes reviewForm submit event listener
+        }); //closes reviewForm submit-event 
         
-
-
-        //reset reviews button event listener
+        
         resetReviewsBtn.addEventListener('click', (e) => {
         
           reviewsUl.innerHTML = '';
           
-        })//closes resetReviewsBtn click event listener
+        })//closes resetReviewsBtn click-event 
 
-
-
+        showPeopleBtn.addEventListener('click', (e) => {
+          people(URL)
+        })//closes showPeople click-event 
 
       })
       .catch((err) => {
         console.error(err);
       }); // closes fetch call block
-    
-    
-
-  
-
-    
+   
 } //closes funtion fetchData
 
 
